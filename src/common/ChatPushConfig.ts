4b534ec0046c0c484a46d32de64c544cf37175c8
asteriskzuo <zuoyuhsywn@hotmail.com>
2022-07-08 11:37:05 +0800

    update api references

--- src/common/ChatPushConfig.ts
+++ src/common/ChatPushConfig.ts
@@ -3,11 +3,6 @@ import { Platform } from 'react-native';
 /**
  * The push configuration class.
  * Google Firebase Cloud Messaging information. See {@url https://firebase.google.com/docs/cloud-messaging/concept-options}
- * Xiaomi devices, See {@url https://dev.mi.com/console/doc/detail?pId=863}
- * Oppo devices, See {@url https://open.oppomobile.com/new/introduction?page_name=oppopush}
- * Vivo devices, See {@url https://dev.vivo.com.cn/documentCenter/doc/541}
- * Meizu devices, See {@url https://open.flyme.cn/open-web/views/push.html}
- * Huawei devices, See {@url https://developer.huawei.com/consumer/cn/doc/development/HMSCore-Guides/service-introduction-0000001050040060}
  * Apple devices, See {@url https://developer.apple.com/documentation/usernotifications}
  * React Native identifies platform information, See {@url https://reactnative.dev/docs/platform}
  * For example:
@@ -15,8 +10,6 @@ import { Platform } from 'react-native';
  * Print result:
  *    ```
  *    [
- *       {"Brand": "Meizu", "Fingerprint": "Meizu/meizu_MX6/MX6:6.0/MRA58K/1474961742:user/release-keys", "Manufacturer": "Meizu", "Model": "MX6", "Release": "6.0", "Serial": "M95QACPC6LD49", "Version": 23, "isTesting": false, "reactNativeVersion": {"major": 0, "minor": 66, "patch": 4, "prerelease": null}, "uiMode": "normal"},
- *       {"Brand": "HUAWEI", "Fingerprint": "HUAWEI/ELE-AL00/HWELE:10/HUAWEIELE-AL00/10.1.0.162C00:user/release-keys", "Manufacturer": "HUAWEI", "Model": "ELE-AL00", "Release": "10", "Serial": "unknown", "Version": 29, "isTesting": false, "reactNativeVersion": {"major": 0, "minor": 66, "patch": 4, "prerelease": null}, "uiMode": "normal"},
  *       {"Brand": "google", "Fingerprint": "google/walleye/walleye:11/RP1A.200720.009/6720564:user/release-keys", "Manufacturer": "Google", "Model": "Pixel 2", "Release": "11", "Serial": "unknown", "Version": 30, "isTesting": false, "reactNativeVersion": {"major": 0, "minor": 66, "patch": 4, "prerelease": null}, "uiMode": "normal"}
  *   ]
  *    ```
@@ -26,11 +19,6 @@ export class ChatPushConfig {
    * The device ID.
    * If it is google, the field is FCM send ID.
    * If it is Apple, this field is the certificate name.
-   * If it is Huawei, this field is App ID.
-   * If it is Xiaomi, the field is App ID.
-   * If it is Oppo, this field is App Key.
-   * If it is Meizu, the field is App ID.
-   * If it is Vivo, this field is App ID.
    */
   deviceId?: string;
   /**
