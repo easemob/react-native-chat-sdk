50e7a8b01453dc169a3481091f674e35a0d92919
asteriskzuo <zuoyuhsywn@hotmail.com>
2022-08-05 15:20:28 +0800

    update push demo

--- examples/firebase_push_demo/index.js
+++ examples/firebase_push_demo/index.js
@@ -2,8 +2,18 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Alert} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import messaging from '@react-native-firebase/messaging';
+
+console.log('start...');
+
+messaging().onMessage(async remoteMessage => {
+  Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+});
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
 
 AppRegistry.registerComponent(appName, () => App);
