310c997a3ba321073f484899dafe49f701de1939
asteriskzuo <zuoyuhsywn@hotmail.com>
2023-11-18 16:09:10 +0800

    docs: update

--- src/common/ChatContact.ts
+++ src/common/ChatContact.ts
@@ -1,5 +1,14 @@
+/**
+ * The Contact class is used to store the information of a contact.
+ */
 export class ChatContact {
+  /**
+   * The user id of the contact.
+   */
   userId: string;
+  /**
+   * The remark of the contact.
+   */
   remark: string;
   constructor(params: { userId: string; remark: string }) {
     this.userId = params.userId;
